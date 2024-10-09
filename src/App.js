9232bbada48c931d11c1b27ff0c8bc5a74c6abe1
gansta10 <thanhminh09378@gmail.com>
2024-10-09 10:30:20 +0700

    riconricha

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import NewEventFolder from './components/NewEventFolder';
+
+function App() {
+  return (
+    <div className = "App">
+      <Header/>
+      <NewEventFolder/>
+    </div>
+  );
+}
+
+export default App;
